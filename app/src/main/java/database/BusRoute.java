0f8c7e396a7a287e85526c457903154a553d3288
nrannou <nicolas.rannou@etudiant.univ-rennes1.fr>
2017-11-26 17:17:33 +0100

    création des commandes BDD

---
+++ app/src/main/java/database/BusRoute.java
@@ -0,0 +1,64 @@
+package database;
+
+/**
+ * Created by nicolas on 26/11/2017.
+ */
+
+public class BusRoutes {
+
+    private String routeShortName;
+    private String routeLongName;
+    private String routeDescritpion;
+    private String routetype;
+    private String routeColor;
+    private String routeTextColor;
+
+    public String getRouteShortName() {
+        return routeShortName;
+    }
+
+    public void setRouteShortName(String routeShortName) {
+        this.routeShortName = routeShortName;
+    }
+
+    public String getRouteLongName() {
+        return routeLongName;
+    }
+
+    public void setRouteLongName(String routeLongName) {
+        this.routeLongName = routeLongName;
+    }
+
+    public String getRouteDescritpion() {
+        return routeDescritpion;
+    }
+
+    public void setRouteDescritpion(String routeDescritpion) {
+        this.routeDescritpion = routeDescritpion;
+    }
+
+    public String getRoutetype() {
+        return routetype;
+    }
+
+    public void setRoutetype(String routetype) {
+        this.routetype = routetype;
+    }
+
+    public String getRouteColor() {
+        return routeColor;
+    }
+
+    public void setRouteColor(String routeColor) {
+        this.routeColor = routeColor;
+    }
+
+    public String getRouteTextColor() {
+        return routeTextColor;
+    }
+
+    public void setRouteTextColor(String routeTextColor) {
+        this.routeTextColor = routeTextColor;
+    }
+
+}
---
+++ app/src/main/java/database/Trip.java
@@ -0,0 +1,8 @@
+package database;
+
+/**
+ * Created by nicolas on 26/11/2017.
+ */
+
+public class Trip {
+}
